936d5559247cd74435ddd925d045dd4f1cd4f059
Deathvanos <char.mailley@gmail.com>
2024-04-16 15:32:18 +0200

    First Commit

---
+++ Projet_Danmachi/src/main/java/com/isep/appli/ProjetDanmachiApplication.java
@@ -0,0 +1,13 @@
+package com.isep.appli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDanmachiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDanmachiApplication.class, args);
+	}
+
+}
