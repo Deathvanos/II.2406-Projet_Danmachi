6abb6480c8cc99ae4d508ff7c173a0277c83cff0
lenaf0n <lenafondeur96@gmail.com>
2024-04-22 14:13:45 +0200

    add to controller

--- Projet_Danmachi/src/main/java/com/isep/appli/controllers/UserController.java
+++ Projet_Danmachi/src/main/java/com/isep/appli/controllers/UserController.java
@@ -1,6 +1,9 @@
 package com.isep.appli.controllers;
 
+import com.isep.appli.models.Persona;
+import com.isep.appli.services.PersonaService;
 import com.isep.appli.services.EmailService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.isep.appli.models.User;
@@ -16,16 +20,19 @@ import com.isep.appli.services.UserService;
 
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 @Controller
 public class UserController {
 	
 	private final UserService userService;
-	
 	private final EmailService emailService;
+	private final PersonaService personaService;
 
-	UserController(UserService userService, EmailService emailService) {
+	UserController(UserService userService, EmailService emailService, PersonaService personaService) {
 		this.userService = userService;
 		this.emailService = emailService;
+		this.personaService = personaService;
 	}
 	
 	/*******************************************************************************/
@@ -83,15 +90,16 @@ public class UserController {
 	}
 	
 	@PostMapping("/login") 
-	public String checkLogin(@Valid User user, BindingResult result, Model model) {
+	public String checkLogin(@Valid User user, BindingResult result, Model model, HttpSession session) {
 		User userSignedIn = userService.login(user.getEmail(), user.getPassword());
 
 		if (userSignedIn == null) {
 			model.addAttribute("loginError", true);
 			return "/login";
 		}
+		session.setAttribute("user", userSignedIn);
 
-		return "redirect:/homePlayer";
+		return "redirect:/user-profile";
 	}
 	
 	
@@ -99,8 +107,30 @@ public class UserController {
 	/******************************** PLAYER ***************************************/
 	/*******************************************************************************/
 
-	@GetMapping("/homePlayer") 
-	public String checkLogin() {
-		return "homePlayer";
+	@GetMapping("/user-profile")
+	public String checkLogin(Model model, HttpSession session) {
+
+		User user = (User) session.getAttribute("user");
+		List<Persona> personas = personaService.getPersonasByUser(user);
+
+		model.addAttribute("user", user);
+		model.addAttribute("personas", personas);
+		model.addAttribute("persona", new Persona());
+		return "user-profile";
+	}
+
+	@PostMapping("/save-persona")
+	public String savePersonaToUser(@Valid Persona persona,
+									@RequestParam("file") MultipartFile file,
+									HttpSession session,
+									Model model) {
+		User user = (User) session.getAttribute("user");
+
+		if (personaService.savePersona(file, user, persona)) {
+			return "redirect:/user-profile";
+		}
+
+		model.addAttribute("createPersonaError", true);
+		return "user-profile";
 	}
 }
