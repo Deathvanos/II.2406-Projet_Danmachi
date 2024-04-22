509db02dd1f048dadd0446d0b5b9f72affc233a9
lenaf0n <lenafondeur96@gmail.com>
2024-04-22 15:40:42 +0200

    fix front

--- Projet_Danmachi/src/main/java/com/isep/appli/models/Race.java
+++ Projet_Danmachi/src/main/java/com/isep/appli/models/Race.java
@@ -1,9 +1,17 @@
 package com.isep.appli.models;
 
 public enum Race {
-    HUMAN("humain"),
-    AMAZON("Amazonne"),
-    BEAST("homme-bête");
+    HUMAN("Humain"),
+    AMAZONESS("Amazonne"),
+    BOAZ("Homme-bête"),
+    DARKELF("Elfe noir"),
+    DWARF("Nain"),
+    ELF("Elfe"),
+    GOD("Dieu"),
+    HUMEBUNNY("Lapin hume"),
+    PALLUM("Pallum"),
+    SPIRIT("Esprit"),
+    XENOS("Xenos");
 
     private final String displayName;
 
