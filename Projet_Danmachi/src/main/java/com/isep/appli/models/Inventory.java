85eff97f17762f7610ec95b122a9fd3df78c6aa2
cerma <cermakhugo@gmail.com>
2024-04-29 14:36:06 +0200

    Add-Inventory View

---
+++ Projet_Danmachi/src/main/java/com/isep/appli/models/Inventory.java
@@ -0,0 +1,29 @@
+package com.isep.appli.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "inventory")
+public class Inventory implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private Long idItem;
+	private Long idPlayer;
+	private int quantity;
+
+	@Override
+	public String toString() {
+		return "Item [id=" + id + ", idItem=" + idItem + "idPlayer=" + idPlayer + "quantity=" + quantity
+				+ "]";
+	}
+}
