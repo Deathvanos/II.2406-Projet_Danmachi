c3b1ae509c3fe868edc561d79198a5dade439107
marieda92 <marie.deamorim@orange.fr>
2024-06-06 22:50:20 +0200

    Join requests + delete members

--- src/main/java/com/isep/appli/services/FamiliaService.java
+++ src/main/java/com/isep/appli/services/FamiliaService.java
@@ -9,21 +9,19 @@ import org.springframework.stereotype.Service;
 
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
 public class FamiliaService {
-    @Autowired
-    private ImageService imageService;
     @Autowired
     private FamiliaRepository familiaRepository;
 
     @Autowired
     private PersonnageRepository personnageRepository;
 
-    FamiliaService(FamiliaRepository familiaRepository, ImageService imageService) {
+    FamiliaService(FamiliaRepository familiaRepository) {
         this.familiaRepository = familiaRepository;
-        this.imageService = imageService;
         this.personnageRepository = personnageRepository;
     }
 
@@ -58,6 +56,25 @@ public class FamiliaService {
         personnage.setFamilia(familia);
         personnageRepository.save(personnage);
         return true;
+    }
+
+    public void removeMember(Long familiaId, Long memberId) {
+        Familia familia = familiaRepository.findById(familiaId).orElse(null);
+        if (familia != null) {
+            List<Personnage> members = personnageRepository.findByFamiliaId(familiaId);
+            Personnage memberToRemove = members.stream()
+                    .filter(member -> member.getId().equals(memberId))
+                    .findFirst()
+                    .orElse(null);
+            if (memberToRemove != null) {
+                memberToRemove.setFamilia(null); // Mettre à null le ID de famille du membre
+                personnageRepository.save(memberToRemove); // Enregistrer les modifications du personnage
+            } else {
+                throw new IllegalArgumentException("Le personnage spécifié n'est pas membre de cette famille.");
+            }
+        } else {
+            throw new IllegalArgumentException("La famille spécifiée n'existe pas.");
+        }
     }
 
 }
